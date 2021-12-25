2adfa87062df9017d75b43267960289886742346
Rupesh Kumar <111901045@smail.iitpkd.ac.in>
2021-12-25 07:21:01 +0530

    few hanges to the file heirarchy of the todolist

--- todo_react/src/App.js
+++ todo_react/src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+// making the todolist using the react 
+import React from 'react';
+import "./index.css"
 
+// defining the function component of the app 
 function App() {
+  console.log("this is app component");
+
+  // say everything went fine 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <div className="main_div">
+        <div className="center_div">
+          <h1>ToDoList</h1>
+          <input type="text" placeholder='Add New Item' />
+          <button> + </button>
+
+          {/* now we have to add the component for the todolist list  */}
+          <ul>
+            <li>Buy the vegetables from the market</li>
+          </ul>
+        </div>
+      </div>
+    </>
+  )
 }
 
+// exporting this to other index.js file 
 export default App;
